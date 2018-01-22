3e35072b5de7faa4685726ff0f221c468acdf7e4
LenaEriksson <lena.domsten@gmail.com>
2018-01-22 14:18:49 +0100

    app can run locally

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 (function () {
-    "use strict";
+  'use strict'
 
-    // modules
-    var express = require('express');
-    var app = express();
-    var server = require('http').Server(app);
-    var routes = require('./app/routes/routes.js');
-    var bodyParser = require('body-parser');
+  // modules
+  let express = require('express')
+  let app = express()
+  let server = require('http').Server(app)
+  let routes = require('./app/routes/routes.js')
+  let bodyParser = require('body-parser')
 
-    // Start the server to listen for incoming requests on port 9090.
-    server.listen(9090);
+  // Start the server to listen for incoming requests on port 9090.
+  server.listen(9090)
 
-    app.use(bodyParser.json());
+  app.use(bodyParser.json())
 
-    // Static files in the public folder
-    app.use(express.static(__dirname + '/client'));
+  // Static files in the public folder
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, '/client')))
 
-    // Wire up the router.
-    app.use('/', routes);
+  // Wire up the router.
+  app.use('/', routes)
 
     // Create the app module
-    module.exports = app;
-}());
+  module.exports = app
+}())
