3215a76f1d4b6585767ce9b5b024bd633fbb817e
LenaEriksson <lena.domsten@gmail.com>
2018-02-17 08:10:29 +0100

    tidied up code for remove book

--- app/resources/RemoveBookResource.js
+++ app/resources/RemoveBookResource.js
@@ -10,33 +10,27 @@
         var books = [];
         var xmlfile = __dirname + "/../../books.xml";
         var parser = new xml2js.Parser();
+
         fs.readFile(xmlfile, "utf-8", function (error, text) {
         if (error) {
             throw error;
         } else {
-            parser.parseString(text, function (err, result) {
-               
+            parser.parseString(text, function (err, result) {                       
                 var xmlBooks = result['catalog']['book'];
 
-                var removeIndex = -1;
                 for (var i=0; i<xmlBooks.length; i++) {
-                    var test = result.catalog.book[i].$.id;
-                    if (test == id) {
-                        removeIndex = i;    
+                    if (result.catalog.book[i].$.id == id) {
+                        result.catalog.book.splice(i,1);
                     }
                 }
 
-                result.catalog.book.splice(removeIndex,1)
-
-                var builder = new xml2js.Builder();
-                var xml = builder.buildObject(result);
-
-                var updatedXmlBooks = result['catalog']['book'];
-            
-                for (var i=0; i<updatedXmlBooks.length; i++) {
+                for (var i=0; i<xmlBooks.length; i++) {
                     books.push(new Book(xmlBooks[i].$.id, xmlBooks[i].title[0], xmlBooks[i].author[0], xmlBooks[i].genre[0], xmlBooks[i].publish_date[0], xmlBooks[i].price[0], xmlBooks[i].description[0]));
                 }
-
+                
+                var builder = new xml2js.Builder();
+                var xml = builder.buildObject(result);
+            
                 fs.writeFile(xmlfile, xml, function(error, data){
                     if (error) {
                         throw error;
